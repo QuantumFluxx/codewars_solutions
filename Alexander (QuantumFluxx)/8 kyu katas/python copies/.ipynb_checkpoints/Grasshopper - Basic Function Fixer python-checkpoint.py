9013d41e52f33358b005113a3b332ff216797f0c
Alexander <abyss5041@gmail.com>
2022-07-26 18:43:23 +0300

    loaded 8kyu katas solutions

---
+++ Alexander (QuantumFluxx)/8 kyu katas/python copies/.ipynb_checkpoints/Grasshopper - Basic Function Fixer python-checkpoint.py
@@ -0,0 +1,37 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "3f4b78e6",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def add_five(num):\n",
+    "    total = num + 5\n",
+    "    return total"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
---
+++ Alexander (QuantumFluxx)/8 kyu katas/python copies/.ipynb_checkpoints/Regexp Basics - is it a digit python-checkpoint.py
@@ -0,0 +1,36 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "8ebf1112",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def is_digit(n):\n",
+    "    return n.isdigit() and len(n)==1"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
