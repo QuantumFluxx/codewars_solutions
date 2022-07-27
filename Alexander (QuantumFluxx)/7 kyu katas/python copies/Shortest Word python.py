67264b3a07685f101c8568bb62a8275d18ae0f45
Alexander <abyss5041@gmail.com>
2022-07-27 11:35:42 +0300

    loaded 8 and 7 kyu solutions

---
+++ Alexander (QuantumFluxx)/7 kyu katas/python copies/Shortest Word python.py
@@ -0,0 +1,36 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "924b092e",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def find_short(s):\n",
+    "    return min(len(x) for x in s.split())"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
