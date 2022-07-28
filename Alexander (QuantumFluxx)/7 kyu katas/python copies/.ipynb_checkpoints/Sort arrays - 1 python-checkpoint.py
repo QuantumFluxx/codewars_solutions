9b67fcae9acb1be74b1a9e8787e6db97c99da8f0
Alexander <abyss5041@gmail.com>
2022-07-28 21:20:52 +0300

    load 7 kyu katas solutions

---
+++ Alexander (QuantumFluxx)/7 kyu katas/python copies/.ipynb_checkpoints/Sort arrays - 1 python-checkpoint.py
@@ -0,0 +1,40 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "1bb531cd",
+   "metadata": {},
+   "source": [
+    "## DESCRIPTION:\n",
+    "Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.\n",
+    "\n",
+    "## Solution:\n",
+    "```python\n",
+    "def sortme(names):\n",
+    "    return sorted(names)\n",
+    "```"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
