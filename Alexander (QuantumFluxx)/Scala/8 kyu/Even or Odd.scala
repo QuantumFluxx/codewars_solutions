bc41d51652523fc2c1df0849f334b6d213e43150
Alexander <abyss5041@gmail.com>
2022-07-31 14:03:02 +0300

    add scala solutions 8 kyu and python 8 kyu

---
+++ Alexander (QuantumFluxx)/Scala/8 kyu/Even or Odd.scala
@@ -0,0 +1,10 @@
+object Sol {
+
+  def evenOrOdd(number: Int): String = {
+    if( number % 2 == 0 ){
+      return("Even")
+    } else{
+      return("Odd")
+    }
+  }
+}
---
+++ Alexander (QuantumFluxx)/Scala/8 kyu/Multiply.sc
@@ -0,0 +1,3 @@
+object Multiply {
+  def multiply(a: Int, b: Int) = a * b
+}
